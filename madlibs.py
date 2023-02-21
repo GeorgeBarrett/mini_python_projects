aaa0facdf6e2d4df4ef4c38353a3937c9ba32f2f
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-02-21 11:50:00 +0000

    basic madlib game completed

---
+++ madlibs.py
@@ -0,0 +1,15 @@
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+adj2 = input("Adjective: ")
+noun = input ("Noun: ")
+planet = input("Planet: ")
+adj2 = input("Adjective: ")
+
+
+
+madlib = f"The universe is so {adj}! It fascinates me because, I love to {verb1}. \
+{verb2} at the stars at night and pretend to be a {noun}. My favourite \
+planet is {planet} because it is {adj2}."
+
+print(madlib)
